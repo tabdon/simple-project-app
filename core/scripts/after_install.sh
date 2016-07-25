c98a49d15c0098256613b8aede556b5ad260a9e4
Tobias Abdon <tobias@revomac.local>
2016-07-25 14:54:06 +0700

    added inital data

--- core/scripts/after_install.sh
+++ core/scripts/after_install.sh
@@ -4,5 +4,6 @@ sudo echo -e "DEBUG=False" > $FILENAME
 sudo echo -e "DATABASES = {'default': {'ENGINE': 'django.db.backends.mysql','HOST': 'mysql.alpha','NAME': 'mainDB','USER': 'dbmaster','PASSWORD': '5T8390sdkd','PORT': '3306',}}" > $FILENAME
 
 sudo python /var/www/manage.py migrate --noinput
+sudo python /var/www/manage.py loaddata /var/www/core/web/fixtures/initial_data.json
 
 sudo chown www-data:www-data -R /var/www
